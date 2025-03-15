170f740f408d75f827810ed25a6ecfc4ee309d80
AlecxDaSen <alecxenderkhdp@gmail.com>
2025-03-15 16:57:06 +0530

    fix the slide issue

--- index.php
+++ index.php
@@ -5,36 +5,34 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rajasinghe Central college</title>
-    <link rel="icon" href="images/hrccbadge.jpg">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="icon" href="images/hrccbadge.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
-
- 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
 
 </head>
 
+
 <body>
 
-    
-    <div class="container-fluid">
 
-        <div class=" col-12 background">
-            <div class="col-8 offset-2 content">
-                <div class="row">
-                    <div class="col-2 offset-1 my-auto">
-                        <img src="images/hrccbadge.jpg" alt="hrccbadge" class="hrccbadge">
-                    </div>
-                    <div class="col-9 ">
-                        <h1 class="text-white">Rajasinghe Central College</h1>
-                    </div>
-                </div>
-            </div>
+
+    <div class="background">
+        <div class="col-12 text-center pt-lg-5">
+            <h1 class="text-uppercase text-white " style="margin-top: 100px;">welcome to </h1>
+            <img src="images/hrccbadge.png" class="hrccbadge ">
+            <h1 class="text-uppercase fw-bold text-white" style="font-size: 72px;">Rajasinghe Central College </h1>
         </div>
 
-        <div class="col-lg-12 head fixed-top pt-2 pb-2">
-            <div class=" row">
+    </div>
+
+    <div class="container-fluid">
+
+        <!-- navigator -->
+
+        <div class="col-lg-12  head fixed-top pt-2 pb-2">
+            <div class="row ">
                 <div class="col-1 my-auto text-center">
                     <a href="">
                         <i class="bi bi-list fs-1 fw-bold text-white"></i>
@@ -42,108 +40,104 @@
                 </div>
 
                 <div class="col-1 text-center ">
-                    <img src="images/hrccbadge.jpg" alt="Hrcc Badge" class="hrccicon mt-lg-2">
+                    <img src="images/hrccbadge.png" alt="Hrcc Badge" class="hrccicon mt-lg-2">
                 </div>
                 <div class="col-3 my-auto">
-                    <h5 class="text-white text-start fs-5 mt-lg-2">Rajasinghe Central <br> College</h5>
+                    <h5 class="text-white text-start fs-4 ">Rajasinghe Central College</h5>
                 </div>
 
-                <div class="col-4 offset-1 my-auto ">
-                    <input type="text" class="form-control bg-dark rounded-pill" placeholder="Search..." id="">
-                </div>
 
-                <div class="col-1 my-auto text-start">
+                <div class="col-lg-1 offset-lg-6 my-auto text-center">
                     <a href="">
                         <i class="bi bi-search fs-3 fw-bold text-white"></i>
                     </a>
                 </div>
-                <div class="col-1 text-start my-auto">
-                    <button class=" btn btn-outline-secondary rounded-pill">Sign Up</button>
-                </div>
+
 
             </div>
         </div>
+        <!-- navigator -->
+
+
+
 
         <!-- slide1 start -->
 
-        <div class="col-12  mt-5 ">
+        <div class="col-lg-12  mt-lg-5 ">
 
-            <div class="col-12 text-center">
-                <h1 class="text-secondary">Latest News</h1>
+            <div class="col-12 text-center " style="margin-top: 100px;">
+                <h1 class="text-secondary text-uppercase">Latest News</h1>
                 <p class="fs-5">Recently updated news and Regular activities.</p>
             </div>
 
 
-            <div class="swiper-container">
-                <!-- Slider main container -->
-
-
-            <!-- Slider main container -->
-            <div class="swiper">
-                <!-- Additional required wrapper -->
-                <div class="swiper-wrapper">
-                    <!-- Slides -->
-                    <div class="swiper-slide" style="height: 600px;width: 350px;background-size:max-content" > 
-                        <img src="images/slide_1.jpg" alt="">
-                        <div class="title">
-                            <span>Sport News</span>
-                        </div>
-                    </div>
-
-                    <!-- Slides -->
-                    <div class="swiper-slide"  style="height: 600px;width: 350px;background-size:max-content">
-                        <img src="images/slide_2.jpg" alt="">
-                        <div class="title">
-                            <span>Ongoing Activity </span>
-                        </div>
-                    </div>
-
-                    <!-- Slides -->
-                    <div class="swiper-slide"  style="height: 600px;width: 350px;background-size:max-content">
-                        <img src="images/slide_3.jpg" alt="">
-                        <div class="title">
-                            <span>Event News</span>
-                        </div>
-                    </div>
-
-                    <!-- Slides -->
-                    <div class="swiper-slide"  style="height: 600px;width: 350px;background-size:max-content">
-                        <img src="images/slide_4.jpg" alt="">
-                        <div class="title">
-                            <span>Daily News</span>
-                        </div>
-                    </div>
-
-                    <!-- Slides -->
-                    <div class="swiper-slide"  style="height: 600px;width: 350px;background-size:max-content">
-                        <img src="images/slide_5.jpg" alt="">
-                        <div class="title">
-                            <span>Daily News</span>
-                        </div>
-                    </div>
-
-                </div>
-                <!-- If we need pagination -->
-                <div class="swiper-pagination"></div>
-
-
-
-            </div>
-            </div>
-           
 
         </div>
 
-         
 
+        <!-- slide1 start -->
+        <!-- Slider main container -->
+        <div class="swiper">
+            <!-- Additional required wrapper -->
+            <div class="swiper-wrapper">
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="title">
+                        <span>Sport News</span>
+                    </div>
+                    <img src="images/slide_1.jpg" alt="">
+                </div>
+
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="title">
+                        <span>Ongoing Activity </span>
+                    </div>
+                    <img src="images/slide_2.jpg" alt="">
+                </div>
+
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="title">
+                        <span>Event News</span>
+                    </div>
+                    <img src="images/slide_3.jpg" alt="">
+                </div>
+
+                <!-- Slides -->
+                <div class="swiper-slide">
+                    <div class="title">
+                        <span>Daily News</span>
+                    </div>
+                    <img src="images/slide_4.jpg" alt="">
+                </div>
+
+
+
+            </div>
+            <!-- If we need pagination -->
+            <div class="swiper-pagination"></div>
+
+
+
+        </div>
         <!-- slide1 end -->
+
+        <div class="col-lg-12 text-center align-content-center">
+            <h1 class="fw-bold text-secondary pt-5 text-uppercase">Our Vision</h1>
+
+            
+
+        </div>
+
+
+
 
     </div>
 
 
 
     <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
     <script src="script.js"></script>
 </body>
 
