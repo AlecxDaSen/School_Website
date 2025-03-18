eaa258a31b0fb320983ff42561e3e727012c8327
AlecxDaSen <alecxenderkhdp@gmail.com>
2025-03-18 12:57:19 +0530

    new slides and picture issues fixed

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ var swiper = new Swiper(".swiper", {
     effect: "coverflow",
     grabCursor: true,
     centeredSlides: true,
-    initialSlide: 2,
+    initialSlide: 4,
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
@@ -55,3 +55,27 @@ var swiper = new Swiper(".swiper", {
 
 
 });
+
+
+// slide2
+
+const $next = document.querySelector('.next');
+const $prev = document.querySelector('.prev');
+
+$next.addEventListener(
+    'click',
+    ()=>{
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(items[0]);
+   
+    },
+);
+
+$prev.addEventListener(
+    'click',
+    ()=>{
+        const items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(items[items.length -1]);
+    },
+);
+// slide2
